ebc479215410acc46ae00b90c0f0b117da616a7d
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2024-01-03 16:17:55 +0700

    Improved HTML table rendering
    fixes #17

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,3 +46,15 @@
         pagination();
     }
 })();
+
+/* Responsive HTML table */
+(function () {
+    const tables = document.querySelectorAll('.gh-content > table:not(.gist table)');
+    const wrapper = document.createElement('div');
+    wrapper.className = 'gh-table';
+
+    tables.forEach(function (table) {
+        table.parentNode.insertBefore(wrapper, table);
+        wrapper.appendChild(table);
+    });
+})();
