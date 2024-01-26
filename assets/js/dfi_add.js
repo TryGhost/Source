ddd0e6f1b33aa6491c0d0b2af38e6e8cf1033563
Samarth Bansal <samarthbansal42@gmail.com>
2024-01-26 18:02:20 +0530

    initial commit for making toc responsive (some bugs exist)

---
+++ assets/js/dfi_add.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Reference to the menu button and sidebar
+  const menuButton = document.getElementById("menu-button");
+  const sidebar = document.querySelector(".gh-sidebar");
+  const closeButton = document.querySelector(".close-button");
+
+  // Function to close the sidebar
+  function closeSidebar() {
+    sidebar.style.width = "0px";
+    sidebar.style.display = "none";
+    sidebar.classList.remove("active");
+    if (closeButton) {
+      closeButton.style.display = "none";
+    }
+  }
+
+  // Function to handle sidebar toggling
+  function toggleSidebar() {
+    const closeButton = sidebar.querySelector(".close-button");
+    console.log(sidebar.outerHTML);
+    if (
+      sidebar.style.width === "0px" ||
+      sidebar.style.width === "" ||
+      sidebar.style.display === "none"
+    ) {
+      sidebar.style.width = "250px";
+      sidebar.style.display = "block";
+      sidebar.classList.add("active");
+
+      if (closeButton) {
+        closeButton.style.display = "block";
+      }
+      const tocList = sidebar.querySelector(".gh-toc");
+    } else {
+      sidebar.style.width = "0px";
+      sidebar.style.display = "none";
+      sidebar.classList.remove("active");
+      if (closeButton) {
+        closeButton.style.display = "none";
+      }
+    }
+    console.log(sidebar.outerHTML);
+  }
+
+  console.log(menuButton);
+  console.log(closeButton);
+
+  //Event listener to toggle the sidebar
+  menuButton.addEventListener("click", toggleSidebar);
+  closeButton.addEventListener("click", toggleSidebar);
+
+  //Event listener for clicks outside the sidebar
+  document.addEventListener("click", function (event) {
+    if (!sidebar.contains(event.target) && !menuButton.contains(event.target)) {
+      closeSidebar();
+    }
+  });
+
+  // Event listener for window resizing
+  window.addEventListener("resize", function () {
+    if (window.innerWidth >= 1300) {
+      sidebar.style.width = "";
+      sidebar.style.height = "";
+      sidebar.classList.remove("active");
+    } else {
+      // Optionally, you could add some code to handle the mobile view when resizing to a smaller window.
+      // For example, reset the sidebar if you want:
+      sidebar.style.width = "0px";
+      sidebar.style.height = "0px";
+      sidebar.classList.remove("active");
+      if (closeButton) {
+        closeButton.style.display = "none";
+      }
+    }
+  });
+});
