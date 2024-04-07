bb2b22991ecf7a7c2404f99b12755ac9e869c225
Nicolás Georger <info@sredevops.cl>
2024-04-07 02:39:01 -0400

    Update package.json version to 1.2.2, add share-block to post.hbs, add copy.js, add share button functionality to main.js, and add CSS styles for share button.

---
+++ assets/js/copy.js
@@ -0,0 +1,12 @@
+function copyToClipboard() {
+    var dummy = document.createElement('input'),
+    text = window.location.href;
+
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+
+    alert("Link copied to clipboard!");
+}
