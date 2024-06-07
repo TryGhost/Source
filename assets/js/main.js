924b58c59243398ae46ab06e0f7738bc613d9999
Nicolás Georger <34670018+ngeorger@users.noreply.github.com>
2024-06-07 09:25:41 -0400

    Min fixes for sharing blocks ans SVGs

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,15 +60,16 @@
 })();
 
 // Share button
-
 function copyToClipboard() {
-    var dummy = document.createElement("input"),
+    var copiarurl = document.createElement('input'),
     text = window.location.href;
 
-    document.body.appendChild(dummy);
-    dummy.value = text;
-    dummy.select();
-    document.execCommand("copy");
-    document.body.removeChild(dummy);   
+    document.body.appendChild(copiarurl);
+    copiarurl.value = text;
+    copiarurl.select();
+    document.execCommand('copy');
+    document.body.removeChild(copiarurl);
+
     alert("Link copied to clipboard!");
 }
+
