886a0352dee3f0f627136c4cb86bb2a2523a9c0e
Nicolás Georger <34670018+ngeorger@users.noreply.github.com>
2024-07-01 04:27:03 -0400

    refactor: Update share button copy function to use navigator.clipboard

--- gulpfile.js
+++ gulpfile.js
@@ -66,7 +66,7 @@ function js(done) {
 
 const cssWatcher = () => watch('assets/css/**', css);
 const jsWatcher = () => watch('assets/js/**', js);
-const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs'], hbs);
+const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs', 'members/**/*.hbs'], hbs);
 const watcher = parallel(cssWatcher, jsWatcher, hbsWatcher);
 const build = series(css, js);
 
