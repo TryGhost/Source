3ba787dd660c2520339099d21e8b9d75c9ff04cd
kazuya <kazuya@melty.co.jp>
2025-07-17 10:59:02 +0900

    feat: 雑誌トップページの作成

---
+++ assets/js/display-card.js
@@ -0,0 +1,83 @@
+/**
+ * 投稿データからcard.hbsパーシャルと同様のHTMLカードを生成
+ * @param {Object} post - 投稿オブジェクト
+ * @param {string} post.url - 投稿のURL
+ * @param {string} post.title - 投稿のタイトル
+ * @param {string} [post.feature_image] - アイキャッチ画像のURL
+ * @param {string} [post.excerpt] - 投稿の抜粋
+ * @param {string} post.published_at - 公開日時
+ * @param {Array} [post.tags] - タグの配列
+ * @param {Object} [post.group] - グループオブジェクト
+ * @returns {string} HTMLカードの文字列
+ */
+function createPostCard(post) {
+    const featureImage = post.feature_image;
+    const groupLogoImage = post.group?.logo_image;
+    const tags = post.tags ? post.tags.slice(0, 2) : [];
+    const tagHtml = tags.map(tag => `#${tag.name}`).join(' ');
+
+    // 日付をフォーマット
+    const publishedDate = new Date(post.published_at);
+    const formattedDate = publishedDate
+        .toLocaleDateString('ja-JP', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit'
+        })
+        .replace(/\//g, '.');
+
+    return `
+        <article class='card'>
+            <div class='card-image'>
+                ${
+                    featureImage
+                        ? `<img src='${featureImage}' alt='' loading='lazy'>`
+                        : `<img src='/assets/images/default-post-image.png' alt='' loading='lazy'>`
+                }
+                ${
+                    groupLogoImage
+                        ? `
+                    <div class='card-magazine-logo' style='background-image: url(${groupLogoImage})'></div>
+                `
+                        : ''
+                }
+            </div>
+
+            <a href='${post.url}' class='card-content'>
+                <div class='card-header'>
+                    ${
+                        tags.length > 0
+                            ? `
+                        <div class='card-tags'>
+                            ${tagHtml}
+                        </div>
+                    `
+                            : ''
+                    }
+                    <h2 class='card-title'>
+                        ${post.title}
+                    </h2>
+                    ${
+                        post.excerpt
+                            ? `
+                        <p class='card-excerpt'>${post.excerpt}</p>
+                    `
+                            : ''
+                    }
+                </div>
+
+                <div class='card-footer'>
+                    <time class='card-date' datetime='${post.published_at}'>
+                        ${formattedDate}
+                    </time>
+                    <span class='card-readmore'>
+                        続きを読む
+                        <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                            <path d='M9 6l6 6-6 6' stroke='#0c060c' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/>
+                        </svg>
+                    </span>
+                </div>
+            </a>
+        </article>
+    `;
+}
