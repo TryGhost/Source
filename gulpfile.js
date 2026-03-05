57196758d14ed3b1fa0443a460f09c630d63d65a
Steve Larson <9larsons@gmail.com>
2026-03-05 07:04:19 -0600

    Added theme locales/i18n setup
    no ref
    - added @tryghost/theme-translations as a dev dependency
    - updated gulpfile to build locales
    - updated renovate to auto update translation bumps
    
    I suspect the screen.css and source.js file changes are due to dependency bumps.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const pump = require('pump');
 const path = require('path');
 const releaseUtils = require('@tryghost/release-utils');
 const inquirer = require('inquirer');
+const {mergeLocales} = require('@tryghost/theme-translations/build');
 
 // gulp plugins and utils
 const livereload = require('gulp-livereload');
@@ -90,8 +91,9 @@ function zipper(done) {
 const cssWatcher = () => watch('assets/css/**', css);
 const jsWatcher = () => watch('assets/js/**', js);
 const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs'], hbs);
-const watcher = parallel(cssWatcher, jsWatcher, hbsWatcher);
-const build = series(css, js);
+const localesWatcher = () => watch('./locales-local/**/*.json', mergeLocales());
+const watcher = parallel(cssWatcher, jsWatcher, hbsWatcher, localesWatcher);
+const build = series(css, js, mergeLocales());
 
 exports.build = build;
 exports.zip = series(build, zipper);
