e2a5b152250f251e0754e9cdb47361e1a77e180b
Sergio Durzu <sergio.durzu@gmail.com>
2026-08-26 19:11:04 +0200

    Upgrade cosmonauta custom theme (#7)

---
+++ assets/js/custom.js
@@ -0,0 +1,5 @@
+/* Cosmonauta custom JavaScript
+ *
+ * Keep all local JavaScript enhancements in this file. Gulp includes it last
+ * in assets/built/source.js so it can depend on Source's own scripts.
+ */
